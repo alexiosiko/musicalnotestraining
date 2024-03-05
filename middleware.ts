61b22809c21e5f6fc940e2222a44f2b54e9bf4c1
Alexi <jemimassyrup@gmail.com>
2024-03-04 18:22:15 -0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/', 
+  publicRoutes: ['/', '/plans', 
   '/instruments/bouzouki',
   '/instruments/piano',
   '/instruments/flute',
@@ -12,7 +12,7 @@ export default authMiddleware({
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: ['/no-auth-in-this-route',
-  '/', 
+  '/', '/plans', 
   '/instruments/bouzouki',
   '/instruments/piano',
   '/instruments/flute',
