1fcc7aee2067d9da22e999d8491d58c7f5daaede
Alexi <jemimassyrup@gmail.com>
2024-03-09 21:30:53 -0800

    Update route.tsx

--- app/api/payment_success/route.tsx
+++ app/api/payment_success/route.tsx
@@ -1,13 +1,5 @@
 import { NextResponse } from "next/server";
 
-export default function Routee() {
-	
-	return (
-		<div>
-	sucecess
-	</div>
-	)
-}
 
 export async function GET(req: Request) {
 	return new NextResponse(JSON.stringify({ ok: true }), { status: 200 });  
