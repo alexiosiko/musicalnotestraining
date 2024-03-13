dbfcea96e567848e991381fe05d7e6202697314f
Alexi <jemimassyrup@gmail.com>
2024-03-12 20:49:13 -0700

    Update page.tsx

--- app/plans/page.tsx
+++ app/plans/page.tsx
@@ -7,6 +7,7 @@ import { getCustomerId, verifyUser } from '../api/mongodb/userapi';
 import { useUser } from '@clerk/nextjs';
 import { Button } from '@/components/ui/button';
 import { CardTitle } from '@/components/ui/card';
+import { ThreeDots } from 'react-loader-spinner';
 
 
 export default function Plans() {
@@ -25,13 +26,13 @@ export default function Plans() {
 		<div className='m-auto mt-12 gap-12 flex flex-col'>
 			<CardTitle>Buy More Credits!</CardTitle>
 			<div className='grid grid-cols-3 gap-4 justify-center'>
-				{customerId === undefined 
-					&& 
+				{/* {customerId === undefined 
+					&&  */}
 					<div>
-						Fetching data ...
+						<ThreeDots height={20} width={40} />
 
 					</div>
-				}
+				{/* } */}
 				{clerkUser.user && customerId !== undefined && customerId !== null && plans.map((plan, index: number) => 
 					<Plan customerId={customerId} user={clerkUser.user} data={plan} key={index} />
 				)}
