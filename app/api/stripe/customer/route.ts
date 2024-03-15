065c218482e450727fa459a5f96620989ff01c9e
Alexi <jemimassyrup@gmail.com>
2024-03-15 05:05:18 -0700

    Update route.ts

--- app/api/stripe/customer/route.ts
+++ app/api/stripe/customer/route.ts
@@ -6,17 +6,24 @@ export async function POST(req: Request, res: Response) {
 	const body = await req.text();
 	const json = JSON.parse(body);
 	const userId = json.userId;
-	let customer;
 	try {
-		customer = await stripe.customers.search({
-			query: `metadata[\'userId\']:\'${userId}\'`,
-		})
-		console.log(customer.data[0].id);
-		return new Response(JSON.stringify({
-			customerId: customer.data[0].id,
-		}), { status: 200 });
+		const customer = await stripe.customers.list({
+            limit: 1,
+            metadata: {
+                userId: userId
+            }
+        });
+		if (customer.data.length > 0) {
+            return new Response(JSON.stringify({
+                customerId: customer.data[0].id,
+            }), { status: 200 });
+        } else {
+            return new Response(JSON.stringify({
+                message: "Customer not found",
+            }), { status: 404 });
+        }
 	} catch (error: any) {
 		console.error('Error fetching customer:', error);
-		return new Response(JSON.stringify({ message: error.message, customer: customer }), { status: 500 });
+		return new Response(JSON.stringify({ message: error.message }), { status: 500 });
 	}
 }
