df47b344926ab1b4804353ca68545e8ce11304f7
Alexi <jemimassyrup@gmail.com>
2024-03-16 08:07:33 -0700

    Update route.ts

--- app/api/stripe/customer/route.ts
+++ app/api/stripe/customer/route.ts
@@ -6,22 +6,22 @@ export async function POST(req: Request, res: Response) {
 	const json = await req.json();
 	const userId = json.userId;
 	let customers;
+	let customer;
 	try {
-		customers = await stripe.customers.list();
-		if (customers.data[0]) {
-			return Response.json({
-				customerId: customers.data[0].id,
-				customers: customers
-			}, { status: 200 });
-		}
+		customers = await stripe.customers.search({
+			query: `metadata[\'userId\']:\'${userId}\'`,
+		  });
+
+		customer = customers.data.findOne((customer: any) => customer.metadata == userId)
 
 		return Response.json({
 			_customerId: "",
 			userId: userId,
-			customers: customers
+			customers: customers,
+			customer: customer
 		}, { status: 200 });
 	} catch (error: any) {
 		console.error('Error fetching customer:', error);
-		return Response.json({ message: error.message, userId: userId, customers: customers}, { status: 500});
+		return Response.json({ message: error.message, userId: userId, customers: customers, customer: customer}, { status: 500});
 	}
 }
