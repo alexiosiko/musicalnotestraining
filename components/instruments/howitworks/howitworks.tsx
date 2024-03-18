7406ccd8f2c13993597f850e7d3a626d41f0966e
Alexi <jemimassyrup@gmail.com>
2024-03-17 21:41:43 -0700

    Update howitworks.tsx

--- components/instruments/howitworks/howitworks.tsx
+++ components/instruments/howitworks/howitworks.tsx
@@ -4,8 +4,8 @@ import { CardDescription, CardTitle } from '@/components/ui/card';
 export default function HowItWorks () {
 	return (
 		<>
-			{data.map(data => 
-				<div>
+			{data.map((data, index: number) => 
+				<div key={index}>
 					<CardTitle>{data.title}</CardTitle>
 					<CardDescription>{data.descrition}</CardDescription>
 				</div>)}
